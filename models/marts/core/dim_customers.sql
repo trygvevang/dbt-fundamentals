f3818a09cf56abaa9aae8feebbb1b80e1c61fef9
Trygve Nybakk Vang <trygve.nybakk.vang@rikstv.no>
2022-06-02 15:45:39 +0200

    Add model for stripe payments and include in dim_customers

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -11,7 +11,8 @@ customer_orders as (
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recentorder_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
     group by 1
 ),
@@ -23,9 +24,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recentorder_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
 )
 
